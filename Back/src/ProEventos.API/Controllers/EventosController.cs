821c6281ddb076db82169c7608ded277f64125e2
Mateus-S <mateus.gcr.tog@hotmail.com>
2021-12-05 14:47:10 -0300

    Seção 4: Angular

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+using ProEventos.API.Data;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos;
+        }
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(eventos => eventos.EventoId == id);
+        }
+
+    }
+}
+
